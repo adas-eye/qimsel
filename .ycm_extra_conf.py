832988ee24aeb765357f7880e7b1b83c7c3b6a32
S <ricardoliang@gmail.com>
2013-08-09 15:41:01 -0300

    ycm_extra_conf

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,23 @@
+import os
+import ycm_core
+
+flags = [
+'-x', 'c++',
+'-std=c++11',
+'-isystem', '/usr/lib/c++/v1',
+'-I', '/usr/include/qt/QtCore/',
+'-I', '/usr/include/qt/QtGui/',
+'-I', '/usr/include/qt/QtWidgets/',
+'-Wall',
+'-Wextra',
+'-pedantic',
+]
+
+def DirectoryOfThisScript():
+  return os.path.dirname(os.path.abspath(__file__))
+
+def FlagsForFile(filename):
+  return {
+    'flags': flags,
+    'do_cache': True
+  }
